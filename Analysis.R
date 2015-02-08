57e6979c287a82d0e885bd582e3c305c7558ef7b
math.tutor.james <mymath.tutor.james@gmail.com>
2015-02-08 15:46:22 -0600

    Further Data cleaning in Analysis.R

--- Analysis.R
+++ Analysis.R
@@ -2,12 +2,16 @@
 library(plyr)
 library(dplyr)
 library(zoo)
+library(XML)
 
 
 # Load data into R --------------------------------------------------------
 
 #Read in Data file names from "Data" folder
 files <- dir("Data")
+
+#Clear OzData if it already exists to start with a blank slate
+if(exists("OzData")){rm(OzData)}
 
 #for every data file, read it in and add it to OzData.
 for (file in files){
@@ -29,10 +33,45 @@ for (file in files){
    }else{
       OzData <- OzData_tmp
    }
+   print(paste(file,"loaded",sep=" "))
 }
 
+rm(OzData_tmp)
 
-# Do Analysis -------------------------------------------------------------
+# Do Some Cleaning -------------------------------------------------------------
+
+#Sample.Value thinks it is a factor, change it to a character
+#so it stays the correct number, then change to numeric
+OzData$Sample.Value <- as.numeric(as.character(OzData$Sample.Value))
+
+#Paste together Date and hour to get DateTime and make it a POSIXct type
+OzData$DateTime<-paste(OzData$Date,OzData$Start.Time,sep=":")
+OzData$DateTime<-as.POSIXct(OzData$DateTime,format="%Y%m%d:%R")
+
+#Change County.Code to CountyName by Reading in fips table, and Removing the County.Code
+fips<-read.csv("FIPS.csv",header=FALSE,colClasses=c("NULL","NULL","factor","factor","NULL"),
+               col.names=c("State","StateCode","CountyCode","CountyName","ClassCode"))
+
+# merging it with the OzData
+OzData<-merge(OzData,fips,by.x="County.Code",by.y="CountyCode")
+OzData$CountyName <- factor(OzData$CountyName)
+
+# #and Removing the County.Code
+# OzData$County.Code<-NULL
+
+#I want to do the same thing with site codes 
+# doc<-htmlTreeParse("http://www.air.dnr.state.ga.us/amp/data_descrip.html#BLEDSOE",useInternal=TRUE)
+
+# places <- xpathSApply(doc,"//h2",xmlValue)
+
+#Remove a bunch of extra columns
+OzData$Uncertainty <- NULL
+OzData$OzData$Qualifier...9 <- NULL
+OzData$OzData$Qualifier...10 <- NULL
+
+
+rm(fips)
+
 
 
 
