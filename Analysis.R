cbb170fd272cc5adcca4f2de392185c128aa26a1
math.tutor.james <mymath.tutor.james@gmail.com>
2015-02-13 22:26:08 -0600

    Add code to create year and month factors

--- Analysis.R
+++ Analysis.R
@@ -44,9 +44,24 @@ rm(OzData_tmp)
 #so it stays the correct number, then change to numeric
 OzData$Sample.Value <- as.numeric(as.character(OzData$Sample.Value))
 
+#Create Year factor
+OzData$year<-factor(substr(as.character(OzData$Date),1,4))
+
+#Create Month Factor
+OzData$month<-factor(substr(as.character(OzData$Date),5,6))
+
 #Paste together Date and hour to get DateTime and make it a POSIXct type
 OzData$DateTime<-paste(OzData$Date,OzData$Start.Time,sep=":")
 OzData$DateTime<-as.POSIXct(OzData$DateTime,format="%Y%m%d:%R")
+
+#Paste together State.Code, County.Code, and Site.ID to create the full Site ID and save it as a factor back in Site ID
+OzData$Site.ID<-factor(paste(OzData$State.Code,OzData$County.Code,OzData$Site.ID,sep=""))
+
+#Create Common.Name from Lookup table in SiteID.txt
+SiteLookup <- read.csv("SiteID.txt",sep=",",header=T)
+
+OzData<-merge(OzData,SiteLookup,by.x="Site.ID",by.y="Site.ID")
+
 
 #Change County.Code to CountyName by Reading in fips table, and Removing the County.Code
 fips<-read.csv("FIPS.csv",header=FALSE,colClasses=c("NULL","NULL","factor","factor","NULL"),
@@ -66,11 +81,18 @@ OzData$CountyName <- factor(OzData$CountyName)
 
 #Remove a bunch of extra columns
 OzData$Uncertainty <- NULL
-OzData$OzData$Qualifier...9 <- NULL
-OzData$OzData$Qualifier...10 <- NULL
+OzData$Qualifier...2 <- NULL
+OzData$Qualifier...3 <- NULL
+OzData$Qualifier...4 <- NULL
+OzData$Qualifier...5 <- NULL
+OzData$Qualifier...6 <- NULL
+OzData$Qualifier...7 <- NULL
+OzData$Qualifier...8<- NULL
+OzData$Qualifier...9 <- NULL
+OzData$Qualifier...10 <- NULL
 
 
-rm(fips)
+rm(fips,SiteLookup)
 
 
 
